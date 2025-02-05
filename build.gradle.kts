a02c8f3c088bfbb438eab8486feafc557082b54d
crispin <shoy1415@gmail.com>
2025-02-05 14:48:49 +0900

    :tada: Init: project init

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jmailen.gradle.kotlinter.tasks.InstallPreCommitHookTask
+import org.jmailen.gradle.kotlinter.tasks.InstallPrePushHookTask
+
+plugins {
+    kotlin("jvm") version "2.1.0"
+    id("org.jmailen.kotlinter") version "5.0.1"
+}
+
+group = "com.crispin-lab"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
+if (!rootProject.extra.has("install-git-hooks")) {
+    rootProject.extra["install-git-hooks"] = true
+
+    val preCommit: InstallPreCommitHookTask by project.rootProject.tasks.creating(
+        InstallPreCommitHookTask::class
+    ) {
+        group = "build setup"
+        description = "Installs Kotlinter Git pre-commit hook"
+    }
+
+    val prePush: InstallPrePushHookTask by project.rootProject.tasks.creating(
+        InstallPrePushHookTask::class
+    ) {
+        group = "build setup"
+        description = "Installs Kotlinter Git pre-push hook"
+    }
+
+    project.rootProject.tasks.getByName("prepareKotlinBuildScriptModel") {
+        dependsOn(preCommit, prePush)
+    }
+}
