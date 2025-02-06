0fcf6cc7adb2307b53fa860ce0772de1ac37274b
crispin <shoy1415@gmail.com>
2025-02-07 00:00:12 +0900

    :recycle: Refactor: separated method

--- src/main/kotlin/com/crispinlab/Snowflake.kt
+++ src/main/kotlin/com/crispinlab/Snowflake.kt
@@ -28,25 +28,27 @@ class Snowflake(
         private const val DEFAULT_EPOCH: Long = 1735689600000L
 
         private fun createNodeId(): Long {
-            var nodeId: Int
-            try {
-                val networkInterfaces: Enumeration<NetworkInterface> =
-                    NetworkInterface.getNetworkInterfaces()
-                val stringBuilder = StringBuilder()
-                while (networkInterfaces.hasMoreElements()) {
-                    val networkInterface: NetworkInterface = networkInterfaces.nextElement()
-                    val mac: ByteArray? = networkInterface.hardwareAddress
-                    if (mac != null) {
-                        for (macPort: Byte in mac) {
-                            stringBuilder.append(macPort)
-                        }
-                    }
+            val nodeId: Int =
+                try {
+                    nodeIdBuilder()
+                } catch (e: Exception) {
+                    SecureRandom().nextInt()
                 }
-                nodeId = stringBuilder.toString().hashCode()
-            } catch (e: Exception) {
-                nodeId = SecureRandom().nextInt()
-            }
             return nodeId.toLong() and MAX_NODE_ID
+        }
+
+        private fun nodeIdBuilder(): Int {
+            val stringBuilder = StringBuilder()
+            val networkInterfaces: Enumeration<NetworkInterface> =
+                NetworkInterface.getNetworkInterfaces()
+
+            while (networkInterfaces.hasMoreElements()) {
+                val networkInterface: NetworkInterface = networkInterfaces.nextElement()
+                networkInterface.hardwareAddress.let { byteArray: ByteArray ->
+                    byteArray.map { stringBuilder.append(it) }
+                }
+            }
+            return stringBuilder.toString().hashCode()
         }
     }
 
@@ -59,7 +61,16 @@ class Snowflake(
 
     @Synchronized
     fun nextId(): Long {
-        var currentTimestamp: Long = timestamp()
+        val currentTimestamp: Long = handleSequenceGeneration()
+        lastTimestamp = currentTimestamp
+        return currentTimestamp shl
+            (NODE_ID_BITS + SEQUENCE_BITS) or
+            (nodeId shl SEQUENCE_BITS) or
+            sequence
+    }
+
+    private fun handleSequenceGeneration(): Long {
+        val currentTimestamp: Long = timestamp()
 
         require(currentTimestamp >= lastTimestamp) {
             "Invalid System Clock."
@@ -67,25 +78,27 @@ class Snowflake(
 
         if (currentTimestamp == lastTimestamp) {
             sequence = (sequence + 1) and MAX_SEQUENCE
-            if (sequence == 0L) {
-                currentTimestamp = waitNextMillis(currentTimestamp)
-            }
-        } else {
-            sequence = 0
+            return waitForNextTimestamp(currentTimestamp)
         }
+        sequence = 0
 
-        lastTimestamp = currentTimestamp
+        return currentTimestamp
+    }
 
-        return currentTimestamp shl
-            (NODE_ID_BITS + SEQUENCE_BITS) or
-            (nodeId shl SEQUENCE_BITS) or
-            sequence
+    private fun waitForNextTimestamp(currentTimestamp: Long): Long {
+        var timestamp: Long = currentTimestamp
+
+        if (sequence == 0L) {
+            timestamp = waitNextMillis(timestamp)
+        }
+        return timestamp
     }
 
     private fun timestamp(): Long = Instant.now().toEpochMilli() - customEpoch
 
     private fun waitNextMillis(currentTimestamp: Long): Long {
         var timestamp: Long = currentTimestamp
+
         while (timestamp == lastTimestamp) {
             timestamp = timestamp()
         }
@@ -95,7 +108,6 @@ class Snowflake(
     fun parse(id: Long): Array<Long> {
         val maskNodeId: Long = ((1L shl NODE_ID_BITS) - 1) shl SEQUENCE_BITS
         val maskSequence: Long = (1L shl SEQUENCE_BITS) - 1
-
         val timestamp: Long = (id shr (NODE_ID_BITS + SEQUENCE_BITS)) + customEpoch
         val nodeId: Long = (id and maskNodeId) shr SEQUENCE_BITS
         val sequence: Long = id and maskSequence
